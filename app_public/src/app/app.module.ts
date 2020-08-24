819d2935d629c4f523bf88f35bb445678abd4d3a
Juan Chaves <chavesjuan@cityuniversity.edu>
2020-08-24 15:03:04 -0700

    ng add @angular/material && indigo-pink

--- app_public/src/app/app.module.ts
+++ app_public/src/app/app.module.ts
@@ -18,7 +18,8 @@ import { DetailsPageComponent } from './details-page/details-page.component';
 import { LocationDetailsComponent } from './location-details/location-details.component';
 import { MostRecentFirstPipe } from './most-recent-first.pipe';
 import { FilterPipe } from './filter.pipe';
-import { WeatherComponent } from './weather/weather.component'
+import { WeatherComponent } from './weather/weather.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 @NgModule({
   declarations: [
     FrameworkComponent,
@@ -41,7 +42,8 @@ import { WeatherComponent } from './weather/weather.component'
     HttpClientModule,
     FormsModule, 
     ReactiveFormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [FrameworkComponent]
