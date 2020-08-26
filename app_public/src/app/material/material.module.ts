446701287ca17b9986dfde5f7c79c9b57b6c0986
Juan Chaves <chavesjuan@cityuniversity.edu>
2020-08-26 13:43:58 -0700

    not enough time to finish moving to this UI

--- app_public/src/app/material/material.module.ts
+++ app_public/src/app/material/material.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon'; 
 import {MatCardModule} from '@angular/material/card';
 import {MatChipsModule} from '@angular/material/chips';
@@ -9,6 +10,7 @@ import {MatButtonModule} from '@angular/material/button';
 const MaterialComponents = [
   MatGridListModule,
   MatToolbarModule,
+  MatFormFieldModule,
   MatIconModule,
   MatCardModule,
   MatChipsModule,
