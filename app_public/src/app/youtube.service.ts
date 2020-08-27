f27ca96a0957bf1e3f1173aa083fc500af41e702
Juan Chaves <chavesjuan@cityuniversity.edu>
2020-08-26 22:40:20 -0700

    added yt keys

--- app_public/src/app/youtube.service.ts
+++ app_public/src/app/youtube.service.ts
@@ -8,7 +8,8 @@ export class YoutubeService {
 
   constructor(private http:HttpClient) { }
   getChannels(channelName):Observable<any>{
-    const API_KEY="AIzaSyA___1PHslQAe1hkJFjV3V7euIuk4T72II";
+    // const API_KEY="AIzaSyA___1PHslQAe1hkJFjV3V7euIuk4T72II";
+    const API_KEY="AIzaSyB51goP5NlIvQ9ou-HfYOefj2hA6m37cj8";
     const url ="https://www.googleapis.com/youtube/v3/search?part=snippet&q="+channelName+"&type=channel&key="+API_KEY+"&maxResults=2"
     return this.http.get<any>(url);
   }
