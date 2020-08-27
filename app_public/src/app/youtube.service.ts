f9ef5dbdb82493282b2a100f0ec422078f353be7
Juan Chaves <chavesjuan@cityuniversity.edu>
2020-08-26 23:20:49 -0700

    yt search page finished -- now footer bug

--- app_public/src/app/youtube.service.ts
+++ app_public/src/app/youtube.service.ts
@@ -10,7 +10,7 @@ export class YoutubeService {
   getChannels(channelName):Observable<any>{
     // const API_KEY="AIzaSyA___1PHslQAe1hkJFjV3V7euIuk4T72II";
     const API_KEY="AIzaSyB51goP5NlIvQ9ou-HfYOefj2hA6m37cj8";
-    const url ="https://www.googleapis.com/youtube/v3/search?part=snippet&q="+channelName+"&type=channel&key="+API_KEY+"&maxResults=2"
+    const url ="https://www.googleapis.com/youtube/v3/search?part=snippet&q="+channelName+"&type=channel&key="+API_KEY+"&maxResults=6"
     return this.http.get<any>(url);
   }
 }
